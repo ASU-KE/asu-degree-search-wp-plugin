0f95170eb730ae99d660f9fbf06cb51419c67072
John Cox <john.k.cox@asu.edu>
2023-03-08 14:23:44 -0700

    fix areaOfInterest not displaying

--- build/degree-rfi-form/frontend.asset.php
+++ build/degree-rfi-form/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '912acddb5c063e2fa5ff');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1c5347cf42920b91766a');
--- build/degree-rfi-form/index.asset.php
+++ build/degree-rfi-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'afd0ac9318b5076bb0bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eef8bb04393edb748cf6');

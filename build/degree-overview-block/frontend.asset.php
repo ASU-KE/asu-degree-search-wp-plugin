0e3d2af783aff22aaed765ebdb58cfae7b1cc4de
John Cox <john.k.cox@asu.edu>
2023-08-11 16:09:13 -0700

    build

--- build/degree-overview-block/frontend.asset.php
+++ build/degree-overview-block/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'e55bc5a69455dd375c63');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '38ba6c9bf42ba4c11a92');
--- build/degree-overview-block/index.asset.php
+++ build/degree-overview-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9decfcb5be950598f08e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7d040c8ecb739a90f962');

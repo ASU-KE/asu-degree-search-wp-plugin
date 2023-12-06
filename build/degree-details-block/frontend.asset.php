b3907e67bd9578651851809c40979eaace1f2483
John Cox <john.k.cox@asu.edu>
2023-12-06 13:57:19 -0700

    build

--- build/degree-details-block/frontend.asset.php
+++ build/degree-details-block/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1f606bff0383c8a014f4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '91e4bbef1c6464a8004d');

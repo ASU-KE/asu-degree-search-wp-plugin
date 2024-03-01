114ab36cdeafb4c180914f03acbf968aa7ae0a76
John Cox <john.k.cox@asu.edu>
2024-03-01 15:37:56 -0700

    build

--- build/degree-rfi-form/frontend.asset.php
+++ build/degree-rfi-form/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '66bb9ed3a4f68fca07d8');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'e8cb62e35fd8272dca3b');
--- build/degree-rfi-form/index.asset.php
+++ build/degree-rfi-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '650dc9cbfbbf12aefc02');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '83712b281d34dcefd5ba');

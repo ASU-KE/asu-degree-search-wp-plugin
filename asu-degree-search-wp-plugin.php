3112b95ebb91c72a8716d8e8bf2ec71c6128ced7
John Cox <john.k.cox@asu.edu>
2024-03-20 18:58:32 -0700

    bump v1.0.0

--- asu-degree-search-wp-plugin.php
+++ asu-degree-search-wp-plugin.php
@@ -1,18 +1,32 @@
 <?php
 /**
- * Plugin Name:       Asu Degree Search Wp Plugin
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       asu-degree-search
  *
- * @package           create-block
+ *
+ * @package   asu-ke-wp-degree-search
+ * @link      https://github.com/ASU-KE/asu-degree-search-wp-plugin
+ * @author    ASU KE
+ * @copyright 2024 ASU KE
+ * @license   GPL v2 or later
+ *
+ * Plugin Name:  Asu Degree Search Wp Plugin
+ * Description:  The developer tools panel for WordPress.
+ * Version:      1.0.0
+ * Plugin URI:   https://github.com/ASU-KE/asu-degree-search-wp-plugin
+ * Author:       ASU KE
+ * Author URI:   https://github.com/ASU-KE/asu-degree-search-wp-plugin
+ * Text Domain:  asu-degree-search-wp-plugin
+ * Requires PHP: 7.4
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
-
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued

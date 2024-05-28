632cf1d2d81a7e10b4bba71fadd8ca67d13c17e6
John Cox <john.k.cox@asu.edu>
2024-05-28 15:10:52 -0700

    build v2.5.3.1 add PL field options

--- build/degree-details-block/index.asset.php
+++ build/degree-details-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3bffd2e09b2e0de1ad1a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8b687e2b5aac9ecb8147');

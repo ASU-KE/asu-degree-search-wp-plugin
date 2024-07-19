83667a25941546c975c5de0ca0a7a5df09ff8581
John Cox <john.k.cox@asu.edu>
2024-07-19 14:16:16 -0700

    build v1.4.0

--- build/degree-details-block/index.asset.php
+++ build/degree-details-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f8d414fedc44d2694388');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'aba21b0c70b2c7a1cb40');

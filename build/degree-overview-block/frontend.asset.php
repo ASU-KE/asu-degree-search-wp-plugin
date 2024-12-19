567a91fcf5fdf175f619d4355fe28bf107b615e0
John Cox <john.k.cox@asu.edu>
2024-12-19 11:48:27 -0700

    build app-degree-pages v2.6.2

--- build/degree-overview-block/frontend.asset.php
+++ build/degree-overview-block/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '44fb5380faded92c9f16');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '17ca7cf16cf135104e57');
--- build/degree-overview-block/index.asset.php
+++ build/degree-overview-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cb40974f18ed6f977d36');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '79199fd720c61d15681c');

1c1c533aa14f9de68cd26e90968cdfad63dea0ec
John Cox <john.k.cox@asu.edu>
2025-07-30 14:12:12 -0700

    build latest v2.8 app degree pages v3.1.0

--- build/degree-details-block/frontend.asset.php
+++ build/degree-details-block/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '4ad7a50f73fcb9d5df3f');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '48ce3d9b5c71126ae856');
--- build/degree-details-block/index.asset.php
+++ build/degree-details-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '33c69a2e302052fd22d7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '41c568f0a2909a5d21a0');
